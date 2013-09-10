66fe1852ccd1f1f31dc252062ea5cde2178fbc31
Austin Garbelman <solasi77@gmail.com>
2013-09-10 17:40:55 -0400

    Add a very basic hex class, just an outline of what we may want.

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point {
+	public int xPos;
+	public int yPos;
+	public Point(int x, int y)
+	{
+		this.xPos = xPos; this.yPos=yPos;
+	}
+}
+
+public class Hex {
+	public enum resourceGen { Gold, Wood, Iron, light, dark, water };	// Push this out to resource.cs
+	public int population;
+	public string type="";
+	public Material material;
+	public Point position = Point(0,0);
+	//public Player owner;
+	public Object resident;	//Creature or Card instead of Object?
+	public Object[] structures; //Strucutre or Card instead of Object?
+	public int turnsToCapture;
+		
+}
